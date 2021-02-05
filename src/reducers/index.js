47cc6c4ecd1fefc1e43106170aa465560a78cca8
Manuel Desole <desole.manuel@gmail.com>
2021-02-05 05:12:07 +0100

    game details

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import gamesReducer from "./gamesReducer";
 import userReducer from "./userReducer";
+import detailReducer from "./detailReducer";
 
 const rootReducer = combineReducers({
   games: gamesReducer,
   user: userReducer,
+  detail: detailReducer,
 });
 
 export default rootReducer;
